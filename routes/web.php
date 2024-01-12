7eea18f042abb0cc2afa67dc0e41c49a05b0204f
Henrique Silva <henriquersilva.al@gmail.com>
2024-01-12 00:03:26 -0300

    feat: home route, vacancy controller and model created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VacanciesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,13 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/teste', function () {
-    return view('teste');
-});
+Route::get('/', [VacanciesController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
