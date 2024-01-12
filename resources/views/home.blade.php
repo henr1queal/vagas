75f94634e34e3ea4a0e024637b0a12401d5306b6
Henrique Silva <henriquersilva.al@gmail.com>
2024-01-12 00:01:36 -0300

    style: fonts, tamanhos, e header adicionados

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.layout')
+@section('title')
+    Vagas Maceió
+@endsection
+@section('css')
+    <style>
+        
+    </style>
+@endsection
+@section('content')
+    <nav class="py-3 d-flex gap-4 justify-content-center justify-content-md-end px-lg-5">
+        <a class="text-black d-inline-block roboto fs-18 fw-normal" href="http://">Publicar uma vaga</a>
+        <hr class="vr my-0">
+        <a class="text-black d-inline-block roboto fs-18 fw-normal" href="http://">Painel</a>
+    </nav>
+    <header class="row justify-content-center align-items-center">
+        <div class="col text-center">
+            <h1 class="text-white roboto fs-32"><strong>Maceió Vagas</strong></h1>
+            <h2 class="text-white montserrat fs-20 fw-normal mb-0">Conectando você com o futuro.</h2>
+        </div>
+    </header>
+@endsection
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,14 +1,75 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700;800&family=Roboto:wght@400;700&display=swap"
+        rel="stylesheet">
     <title>@yield('title')</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <style>
+        .roboto {
+            font-family: 'Roboto', sans-serif;
+        }
+
+        .montserrat {
+            font-family: 'Montserrat', sans-serif;
+        }
+
+        html {
+            font-size: 62.5%;
+        }
+        
+        .fs-15 {
+            font-size: 1.5rem;
+        }
+        
+        .fs-16 {
+            font-size: 1.6rem;
+        }
+        
+        .fs-18 {
+            font-size: 1.8rem;
+        }
+        
+        .fs-20 {
+            font-size: 2.0rem;
+        }
+        
+        .fs-24 {
+            font-size: 2.4rem;
+        }
+
+        .fs-32 {
+            font-size: 3.2rem;
+        }
+
+        nav {
+            background-color: #CFC6C6;
+        }
+
+        nav .vr {
+            color: #686464;
+            width: 2px;
+            opacity: 0.5;
+        }
+
+        header {
+            height: 25dvh;
+            background-color: #003366;
+        }
+    </style>
+    @yield('css')
 </head>
+
 <body>
     @yield('content')
     @yield('scripts')
 </body>
+
 </html>
