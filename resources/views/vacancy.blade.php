d14e0eb6abcd3695badb4265496d45853fd6d0a6
Henrique Silva <henriquersilva.al@gmail.com>
2024-01-15 18:43:53 -0300

    style: change styles

--- resources/views/vacancy.blade.php
+++ resources/views/vacancy.blade.php
@@ -47,31 +47,31 @@
     <div class="d-flex vacancy-wrapper">
         <aside class="d-none d-lg-flex justify-content-center py-4 pt-lg-5 details-desktop">
             <div class="d-flex flex-column gap-5 text-lg-center" id="details">
-                <h2 class="fs-20 montserrat text-black d-none d-lg-block"><strong>Detalhes sobre a vaga:</strong></h2>
+                <h3 class="fs-20 montserrat text-black d-none d-lg-block"><strong>Detalhes sobre a vaga:</strong></h3>
                 <div class="d-flex flex-column gap-3">
                     <div class="d-flex flex-column">
-                        <h2 class="fs-18 montserrat text-black"><strong>Postada em:</strong></h2>
-                        <h3 class="fs-16 montserrat text-black">{{ $vacancy->created_at->format('d/m/Y') }}</h3>
+                        <h3 class="fs-18 montserrat text-black"><strong>Postada em:</strong></h3>
+                        <h4 class="fs-16 montserrat text-black">{{ $vacancy->created_at->format('d/m/Y') }}</h4>
                     </div>
                     <div class="d-flex flex-column">
-                        <h2 class="fs-18 montserrat text-black"><strong>Empresa:</strong></h2>
-                        <h3 class="fs-16 montserrat text-black">{{ $vacancy->company_name }}</h3>
+                        <h3 class="fs-18 montserrat text-black"><strong>Empresa:</strong></h3>
+                        <h4 class="fs-16 montserrat text-black">{{ $vacancy->company_name }}</h4>
                     </div>
                     <div class="d-flex flex-column">
-                        <h2 class="fs-18 montserrat text-black"><strong>Tipo de trabalho:</strong></h2>
-                        <h3 class="fs-16 montserrat text-black">{{ $vacancy->job_type }}</h3>
+                        <h3 class="fs-18 montserrat text-black"><strong>Tipo de trabalho:</strong></h3>
+                        <h4 class="fs-16 montserrat text-black">{{ $vacancy->job_type }}</h4>
                     </div>
                     <div class="d-flex flex-column">
-                        <h2 class="fs-18 montserrat text-black"><strong>Regime de contratação:</strong></h2>
-                        <h3 class="fs-16 montserrat text-black">{{ $vacancy->employment_type }}</h3>
+                        <h3 class="fs-18 montserrat text-black"><strong>Regime de contratação:</strong></h3>
+                        <h4 class="fs-16 montserrat text-black">{{ $vacancy->employment_type }}</h4>
                     </div>
                     <div class="d-flex flex-column">
-                        <h2 class="fs-18 montserrat text-black"><strong>Carga horária:</strong></h2>
-                        <h3 class="fs-16 montserrat text-black">{{ $vacancy->workload }}h semanais</h3>
+                        <h3 class="fs-18 montserrat text-black"><strong>Carga horária:</strong></h3>
+                        <h4 class="fs-16 montserrat text-black">{{ $vacancy->workload }}h semanais</h4>
                     </div>
                     <div class="d-flex flex-column">
-                        <h2 class="fs-18 montserrat text-black"><strong>Salário:</strong></h2>
-                        <h3 class="fs-16 montserrat text-black">R$ {{ $vacancy->salary }}</h3>
+                        <h3 class="fs-18 montserrat text-black"><strong>Salário:</strong></h3>
+                        <h4 class="fs-16 montserrat text-black">R$ {{ $vacancy->salary }}</h4>
                     </div>
                 </div>
             </div>
@@ -90,15 +90,15 @@
                             </svg>
                         </div>
                         <div class="top-desc d-flex flex-column gap-2 text-center w-100">
-                            <h1 class="title montserrat fs-24 mb-lg-0"><strong>{{ $vacancy->title }}</strong></h1>
-                            <h2 class="fs-19 montserrat text-black d-lg-none"><strong>Detalhes sobre a vaga:</strong></h2>
+                            <h2 class="title montserrat fs-24 mb-lg-0"><strong>{{ $vacancy->title }}</strong></h2>
+                            <h3 class="fs-19 montserrat text-black d-lg-none"><strong>Detalhes sobre a vaga:</strong></h3>
                         </div>
                     </div>
                     <div class="d-flex flex-column gap-3">
                         <div class="details-mobile">
                         </div>
                         <div>
-                            <h3 class="montserrat fs-18 text-black"><strong>Descrição da vaga:</strong></h3>
+                            <h4 class="montserrat fs-18 text-black"><strong>Descrição da vaga:</strong></h4>
                             <p class="montserrat fs-18 text-black fw-medium">{{ $vacancy->description }}</p>
                         </div>
                         <button type="button" class="btn btn-submit text-white fs-18 montserrat py-4"><strong>Eu quero
@@ -109,7 +109,7 @@
                             <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto" width="30" height="30"
                                 fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd"
-                                    d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5" />
+                                    d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5h3.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5" />
                             </svg>
                         </div>
                     </div>
