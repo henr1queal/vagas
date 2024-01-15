49974d70fe38561f37bbf3e0d2a3721e4141ab8c
Henrique Silva <henriquersilva.al@gmail.com>
2024-01-15 13:17:10 -0300

    style: footer add and other changes

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -24,7 +24,15 @@
         html {
             font-size: 62.5%;
         }
-        
+
+        main {
+            background-color: #e5e5e5;
+        }
+
+        footer {
+            background-color: #003366;
+        }
+
         .fs-14 {
             font-size: 1.4rem;
         }
@@ -32,31 +40,31 @@
         .fs-15 {
             font-size: 1.5rem;
         }
-        
+
         .fs-16 {
             font-size: 1.6rem;
         }
-        
+
         .fs-16-mobile {
             font-size: 1.6rem;
         }
-       
+
         .fs-17 {
             font-size: 1.61rem;
         }
-        
+
         .fs-18 {
             font-size: 1.8rem;
         }
-        
+
         .fs-19 {
             font-size: 1.9rem;
         }
-        
+
         .fs-20 {
             font-size: 2.0rem;
         }
-        
+
         .fs-24 {
             font-size: 2.4rem;
         }
@@ -76,7 +84,7 @@
         }
 
         header {
-            height: 20dvh;
+            height: 15dvh;
             background-color: #003366;
         }
 
@@ -85,12 +93,16 @@
                 height: 5dvh;
             }
         }
+
+        footer {
+            height: 5dvh;
+        }
     </style>
     @yield('css')
 </head>
 
 <body class="h-100">
-    <nav class="py-3 d-flex gap-4 justify-content-center align-items-center justify-content-md-end px-lg-5">
+    <nav class="py-3 d-flex gap-4 justify-content-center align-items-center justify-content-md-end px-md-5">
         <a class="text-black d-inline-block roboto fs-18 fw-normal" href="http://">Publicar uma vaga</a>
         <hr class="vr my-0">
         <a class="text-black d-inline-block roboto fs-18 fw-normal me-lg-4" href="http://">Painel</a>
@@ -102,6 +114,20 @@
         </div>
     </header>
     @yield('content')
+    <footer class="align-items-center row m-0 justify-content-end px-lg-5">
+        <div class="col offset-2 text-center">
+            <p class="text-white montserrat fs-16 mb-0">Vagas Maceió - {{ date('Y') }}</p>
+        </div>
+        <div class="col-2 text-end pe-5">
+            <a href="https://www.instagram.com/vagasmaceio.com.br/" target="_blank" rel="noopener noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                    class="bi bi-instagram text-white" viewBox="0 0 16 16">
+                    <path
+                        d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+                </svg>
+            </a>
+        </div>
+    </footer>
     @yield('scripts')
 </body>
 
