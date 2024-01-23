1a572b8a8ea88d857e9c8dffbb21dc625ea88255
Henrique Silva <henriquersilva.al@gmail.com>
2024-01-23 17:04:45 -0300

    fix: webhook except csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'payment.webhook'
+        'webhook-mercado-pago-vaga-change-status'
     ];
 }
