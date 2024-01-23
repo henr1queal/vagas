18331b33bc13ff9d60c25c3503406a0c9ba803b7
Henrique Silva <henriquersilva.al@gmail.com>
2024-01-23 17:11:07 -0300

    fix: change verifycsrftoken to receive webhook

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'webhook-mercado-pago-vaga-change-status'
+        'https://vagasmaceio.com.br/webhook-mercado-pago-vaga-change-status'
     ];
 }
